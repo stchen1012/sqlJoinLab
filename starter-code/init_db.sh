607f4ec886ec785e0a0581103cc8bad05818042d
Tracy Chen <stchen1012@gmail.com>
2019-10-10 20:40:29 -0400

    pushing up lab

--- starter-code/init_db.sh
+++ starter-code/init_db.sh
@@ -2,4 +2,25 @@
 
 # TODO: Create database and initialize database table structure
 
+postgres=# CREATE DATABASE companydata;
+postgres=# \c companydata;
+companydata=# CREATE TABLE employees (
+companydata(# ssn VARCHAR(255),
+companydata(# first_name VARCHAR(255),
+companydata(# last_name VARCHAR(255),
+companydata(# year_of_birth INT,
+companydata(# city VARCHAR(255)
+companydata(# );
+CREATE TABLE
+companydata=# CREATE TABLE jobs (
+companydata(# ssn VARCHAR(255),
+companydata(# company VARCHAR(255),
+companydata(# salary INT,
+companydata(# experience INT
+companydata(# );
+
+
 # TODO: Insert data into tables
+companydata=# COPY employees(ssn, first_name, last_name, year_of_birth, city) FROM '/Users/tracychen/Desktop/sql-joins-lab/starter-code/employees.csv' DELIMITER ',' CSV;
+
+companydata=# COPY jobs FROM '/Users/tracychen/Desktop/sql-joins-lab/starter-code/jobs.csv' DELIMITER ',' CSV;
